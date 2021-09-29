f0370e4eb1c775bb7c672e91007b020145aafd3a
leobusta <leobusta@gmail.com>
2021-09-28 21:48:02 -0500

    lcase 28 sept

---
+++ src/views/Pets.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container grid-list-md fluid>
+        <v-layout wrap>
+            <v-flex xs12 sm4 md3 v-for="dog in dogs" :key="dog.breed">
+                <v-card color="grey lighten-2">
+                    <v-img :src="dog.img" height="170px"></v-img>
+                    <v-card-title>
+                        <div>
+                            <h4>
+                                {{ dog.name }}
+                            </h4>
+                            <p>{{dog.breed}}</p>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+<script>
+import  {Dogs} from '../data/dogs'
+
+export default {
+    data: () => {
+        return {
+            dogs: Dogs
+        }
+    }
+    
+}
+</script>
